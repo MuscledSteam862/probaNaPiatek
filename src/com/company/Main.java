dcf47047dd72b6165b20b0a88555b42cac16480c
Norbert Konefał <norbertkonefa@outlook.com>
2020-12-10 17:04:54 +0100

    Dowolna_wiadomosc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i =0;
+        System.out.println(i);
+	// write your code heressda
     }
 }
